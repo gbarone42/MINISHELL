5414b1ed8f06bdf636c46dc448ed0e1ffe7ea3da
Emma Veronelli <everonel@c2r5p10.42firenze.it>
2023-11-07 20:02:25 +0100

    jjjjjjj

--- src/utils/utils3.c
+++ src/utils/utils3.c
@@ -22,19 +22,18 @@ void *ft_memset(void *b, int c, size_t len)
 // This implementation achieves the same functionality as the previous version but uses a while loop instead of a for loop.
 
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
-	char	*s1;
+    size_t i = 0;
+    char *s1 = (char *)dst;
+    const char *s2 = (const char *)src;
 
-	s1 = (char *)dst;
-	i = 0;
-	while (i < n)
-	{
-		*(char *)s1 = *(char *)src;
-		s1++;
-		src++;
-		i++;
-	}
-	return (dst);
+    while (i < n) {
+        s1[i] = s2[i];
+        i++;
+    }
+
+    return dst;
 }
+
+
