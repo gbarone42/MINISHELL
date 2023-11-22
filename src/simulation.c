132c7ae3408ad99b46c9f0a0e1a11530ec245ace
Emma Veronelli <everonel@c2r4p4.42firenze.it>
2023-11-22 16:19:47 +0100

    builtin

--- src/simulation.c
+++ src/simulation.c
@@ -1,10 +1,101 @@
 #include "../include/minishell.h"
 #include <time.h>
 
-void handle_basic_builtins(t_shell *shell) {
+#ifndef PATH_MAX
+#define PATH_MAX 4096 // You can choose a value that makes sense for your application
+
+#endif
+
+void change_directory(t_shell *shell)
+{
+    char *path; // should be used to store the path to the target directory
+
+    // Skip the "cd" part
+    path = shell->input + 2;
+
+    // Skip leading whitespaces
+    while (*path && (*path == ' ' || *path == '\t'))
+    {
+        path++;
+    }
+
+    if (*path == '\0')
+    {
+        // No path provided, default to home directory
+        path = getenv("HOME");
+    }
+
+    // Implement the logic for changing the directory
+    if (chdir(path) != 0)
+    {
+        perror("cd"); // Print an error message if chdir fails
+    }
+    else
+    {
+        printf("Changed to directory: %s\n", path);
+    }
+
+    // Optionally, update the prompt to show the new directory
+    update_prompt(shell);
+    printf("Prompt: %s\n", shell->prompt);
+}
+
+void update_prompt(t_shell *shell)
+{
+    // Update the prompt based on the new current working directory
+    // For example, you can use getcwd() to get the current working directory
+
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        // Update the prompt with the new current working directory
+        free(shell->prompt); // Free the old prompt
+        shell->prompt = ft_strjoin(PURPLE, getenv("USER")); // You can customize this part
+        shell->prompt = ft_strjoin(shell->prompt, "@");
+        shell->prompt = ft_strjoin(shell->prompt, cwd);
+        shell->prompt = ft_strjoin(shell->prompt, CLR_RMV);
+        shell->prompt = ft_strjoin(shell->prompt, " > ");
+    }
+    else
+    {
+        perror("getcwd");
+    }
+}
+
+// void change_directory(t_shell *shell)
+// {
+//     char *path;
+
+//     // Skip the "cd" part
+//     path = shell->input + 2;
+
+//     // Skip leading whitespaces
+//     while (*path && (*path == ' ' || *path == '\t'))
+//     {
+//         path++;
+//     }
+
+//     // Implement the logic for changing the directory
+//     // You can use chdir() function to change the current working directory
+
+//     if (chdir(path) != 0)
+//     {
+//         perror("cd"); // Print an error message if chdir fails
+//     }
+
+//     // Optionally, update the prompt to show the new directory
+//     // For example, you can update shell->prompt here
+// }
+
+void handle_basic_builtins(t_shell *shell)
+{
     if (!ft_strncmp(shell->input, "exit", 5))
     {
         shell_exit(shell);
+    }
+    else if (!ft_strncmp(shell->input, "cd", 3))
+    {
+        change_directory(shell);
     }
     else if (!ft_strncmp(shell->input, "echo -n", 7))
     {
