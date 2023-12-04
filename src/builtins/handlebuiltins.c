29e9d407a3e9cea2affa68d496a74549b5742a33
Emma Veronelli <everonel@c2r3p8.42firenze.it>
2023-12-04 18:47:26 +0100

    jj

--- src/builtins/handlebuiltins.c
+++ src/builtins/handlebuiltins.c
@@ -47,7 +47,9 @@ void handle_basic_builtin2(t_shell *shell, char **args)
         //handle_export(shell, args);
         handle_export(shell, args);
     }
-    else if (!ft_strncmp(shell->input, "unset", 6))
+    //else if (!ft_strncmp(shell->input, "unset", 6))
+    else if (args && args[0] && !ft_strcmp(args[0], "unset"))
+
     {
         handle_unset(shell, args);
     }
