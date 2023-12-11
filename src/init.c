4f63cdea2beb52f41d843255cc7145c3be7b82fb
badph3 <badph3@gmail.com>
2023-12-11 23:22:51 +0100

    check

--- src/init.c
+++ src/init.c
@@ -1,21 +1,26 @@
 #include "../include/minishell.h"
 
-char **ft_get_env(char **env) {
+char **ft_get_env(char **env)
+{
     char **my_env = NULL;
     int i = 0;
 
     while (env && env[i])
         i++;
     my_env = (char **)malloc(sizeof(char *) * (i + 1));
-    if (!my_env) {
+    if (!my_env)
+    {
         write(STDERR_FILENO, "Memory allocation failed.\n", 27);
         exit(EXIT_FAILURE);
     }
-    for (int j = 0; j < i; j++) {
+    for (int j = 0; j < i; j++)
+    {
         my_env[j] = ft_strdup(env[j]);
-        if (!my_env[j]) {
+        if (!my_env[j])
+        {
             write(STDERR_FILENO, "Memory allocation failed for env variable.\n", 44);
-            while (j > 0) {
+            while (j > 0)
+            {
                 free(my_env[j - 1]);
                 j--;
             }
@@ -27,37 +32,41 @@ char **ft_get_env(char **env) {
     return my_env;
 }
 
-void free_myenv(char **my_env) {
-    if (my_env) {
-        for (int i = 0; my_env[i] != NULL; i++) {
+void free_myenv(char **my_env)
+{
+    if (my_env)
+    {
+        for (int i = 0; my_env[i] != NULL; i++)
+        {
             free(my_env[i]);
         }
         free(my_env);
     }
 }
 
-int ft_innit_user_and_prompt(t_shell *shell, char **env) {
+int ft_innit_user_and_prompt(t_shell *shell, char **env)
+{
     char *user;
     char **env_copied = ft_get_env(env);
 
-    if (!env_copied) {
+    if (!env_copied)
+    {
         write(STDERR_FILENO, "Failed to allocate memory for env_copied.\n", 43);
         return(MEM_ERROR2);
     }
     shell->env = env_copied;
-
     user = ft_strjoin(PURPLE, getenv("USER"));
-    if (!user) {
+    if (!user)
+    {
         write(STDERR_FILENO, "Failed to allocate memory for user.\n", 37);
         free_myenv(env_copied);
         return (MEM_ERROR);
     }
-
     char *prompt_suffix = "@ASHellKETCHUM" CLR_RMV "$ ";
     shell->prompt = ft_strjoin(user, prompt_suffix);
     free(user);
-
-    if (!shell->prompt) {
+    if (!shell->prompt)
+    {
         write(STDERR_FILENO, "Failed to allocate memory for prompt.\n", 38);
         free_myenv(env_copied);
         return(MEM_ERROR3);
@@ -67,11 +76,13 @@ int ft_innit_user_and_prompt(t_shell *shell, char **env) {
     return 0;
 }
 
-int ft_duplicate_std_fds(t_shell *shell) {
+int ft_duplicate_std_fds(t_shell *shell)
+{
     shell->in = dup(STDIN_FILENO);
     shell->out = dup(STDOUT_FILENO);
 
-    if (shell->in == -1 || shell->out == -1) {
+    if (shell->in == -1 || shell->out == -1)
+    {
         write(STDERR_FILENO, "Failed to duplicate file descriptors\n", 38);
         free_myenv(shell->env);
         free(shell->prompt);
@@ -84,7 +95,8 @@ int ft_duplicate_std_fds(t_shell *shell) {
     return 0;
 }
 
-int ft_innit_shell(t_shell *shell, char **env) {
+int ft_innit_shell(t_shell *shell, char **env)
+{
     int result;
 
     result = ft_innit_user_and_prompt(shell, env);
--- src/minishell.c
+++ src/minishell.c
@@ -14,7 +14,8 @@ int main(int ac, char **av, char **env)
     memset(&my_shell, 0, sizeof(t_shell));
     printf("%sWelcome %s!%s\n", GREEN, getenv("USER"), CLR_RMV);
     output = ft_innit_shell(&my_shell, env);
-    if (output != 0) {
+    if (output != 0)
+    {
         perror("Error initializing shell");
         exit(output);
     }
