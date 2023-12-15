dc585f1e637503ee2e472d79eb75979cf4478405
fcorri <corrifilippo@gmail.com>
2023-12-15 17:58:49 +0100

    comments_and_errno

--- src/simulation.c
+++ src/simulation.c
@@ -7,14 +7,9 @@ void ft_minishell_simulator(t_shell *shell)
 
     while (1)
     {
-        errno = 0;
         shell->input = readline(shell->prompt);
-
         if (shell->input == NULL)
-        {
             ft_ctrld(shell);
-        }
-
         if (ft_isvalid(shell->input))
         {
             shell_parser(shell, &command);
@@ -22,9 +17,7 @@ void ft_minishell_simulator(t_shell *shell)
             add_to_history(shell->input);
         }
         else
-        {
          printf("Invalid input detected!\n");
-        }
         free(shell->input);
     }
 }
---
+++ testing/main.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+
+int main(void)
+{
+	char *tmp;
+	tmp = "ciao" " mondo";
+	printf("%s\n", tmp);
+	return (0);
+}
+
