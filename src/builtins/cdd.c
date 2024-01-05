6cc3f9f48f951638a5c810104df5e4f485ae3174
badph3 <badph3@gmail.com>
2024-01-05 21:13:19 +0100

    cleaned_cd

--- src/builtins/cdd.c
+++ src/builtins/cdd.c
@@ -7,129 +7,128 @@
 
 #endif
 
-void ft_handle_cd(t_shell *shell, char **args)
+void	ft_handle_cd(t_shell *shell, char **args)
 {
-    printf("Input for 'cd': %s\n", shell->input);
-    if (args && args[1])
-    {
-        change_directory(shell, args[1]);
-    }
-    else
-    {
-        change_directory(shell, NULL);
-    }
+	printf("Input for 'cd': %s\n", shell->input);
+	if (args && args[1])
+	{
+		change_directory(shell, args[1]);
+	}
+	else
+	{
+		change_directory(shell, NULL);
+	}
 }
 
-void change_directory(t_shell *shell, char *path)
+void	change_directory(t_shell *shell, char *path)
 {
-    if (path == NULL)
-    {
-        path = getenv("HOME");
-    }
-    if (chdir(path) == 0)
-    {   
-        printf("Changed to directory: %s\n", path);
-        update_prompt(shell);
-    }
-    else
-    {
-         perror("cd");
-    }
+	if (path == NULL)
+	{
+		path = getenv("HOME");
+	}
+	if (chdir(path) == 0)
+	{ 
+		printf("Changed to directory: %s\n", path);
+		update_prompt(shell);
+	}
+	else
+	{
+		perror("cd");
+	}
 }
 
-void free_memory(char *user, char *user_at, char *user_at_colon, char *prompt_suffix)
+void	free_memory(char *user, char *user_at, char *user_at_colon, char *prompt_suffix)
 {
-    free(user);
-    free(user_at);
-    free(user_at_colon);
-    free(prompt_suffix);
+	free(user);
+	free(user_at);
+	free(user_at_colon);
+	free(prompt_suffix);
 }
 
-void get_current_directory(char *cwd)
+void	get_current_directory(char *cwd)
 {
-    if (getcwd(cwd, PATH_MAX) == NULL)
-    {
-        perror("getcwd");
-        printf("Error: Unable to get the current working directory\n");
-        exit(EXIT_FAILURE);
-    }
+	if (getcwd(cwd, PATH_MAX) == NULL)
+	{
+		perror("getcwd");
+		printf("Error: Unable to get the current working directory\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
-char* build_user_string(void)
+char*	build_user_string(void)
 {
-    char *user = ft_strjoin(PURPLE, getenv("USER"));
-    if (!user)
-    {
-        perror("Error: Unable to allocate memory for user\n");
-        exit(EXIT_FAILURE);
-    }
-    return user;
+	char	*user = ft_strjoin(PURPLE, getenv("USER"));
+
+	if (!user)
+	{
+		perror("Error: Unable to allocate memory for user\n");
+		exit(EXIT_FAILURE);
+	}
+	return (user);
 }
 
-char* build_user_at_string(char *user)
+char*	build_user_at_string(char *user)
 {
-    char *user_at = ft_strjoin(user, "@ASHellKETCHUM");
+	char	*user_at = ft_strjoin(user, "@ASHellKETCHUM");
+
     if (!user_at)
-    {
-        perror("Error: Unable to allocate memory for user_at\n");
-        free(user);
-        exit(EXIT_FAILURE);
-    }
-    return user_at;
+	{
+		perror("Error: Unable to allocate memory for user_at\n");
+		free(user);
+		exit(EXIT_FAILURE);
+	}
+	return (user_at);
 }
 
-char* build_user_at_colon_string(char *user_at)
+char*	build_user_at_colon_string(char *user_at)
 {
-    char *user_at_colon = ft_strjoin(user_at, ":");
-    if (!user_at_colon)
-    {
-        perror("Error: Unable to allocate memory for user_at_colon\n");
-        free_memory(NULL, user_at, NULL, NULL);
-        exit(EXIT_FAILURE);
-    }
-    return user_at_colon;
+	char	*user_at_colon = ft_strjoin(user_at, ":");
+
+	if (!user_at_colon)
+	{
+		perror("Error: Unable to allocate memory for user_at_colon\n");
+		free_memory(NULL, user_at, NULL, NULL);
+		exit(EXIT_FAILURE);
+	}
+	return (user_at_colon);
 }
 
-char* build_prompt_suffix(void)
+char*	build_prompt_suffix(void)
 {
-    char *prompt_suffix = ft_strjoin(CLR_RMV, "$ ");
-    if (!prompt_suffix)
-    {
-        perror("Error: Unable to allocate memory for prompt_suffix\n");
-        exit(EXIT_FAILURE);
-    }
-    return prompt_suffix;
+	char	*prompt_suffix = ft_strjoin(CLR_RMV, "$ ");
+
+	if (!prompt_suffix)
+	{
+		perror("Error: Unable to allocate memory for prompt_suffix\n");
+		exit(EXIT_FAILURE);
+	}
+	return (prompt_suffix);
 }
 
-void create_prompt(t_shell *shell, char *user_at_colon, char *cwd, char *prompt_suffix)
+void	create_prompt(t_shell *shell, char *user_at_colon, char *cwd, char *prompt_suffix)
 {
-    shell->prompt = (char *)malloc(strlen(user_at_colon) + strlen(cwd) + strlen(prompt_suffix) + 1);
-    if (!shell->prompt)
-    {
-        perror("Error: Unable to allocate memory for the new prompt\n");
-        exit(EXIT_FAILURE);
-    }
-
-    strcpy(shell->prompt, user_at_colon);
-    strcat(shell->prompt, cwd);
-    strcat(shell->prompt, prompt_suffix);
-
-    printf("Updated prompt: %s\n", shell->prompt);
+	shell->prompt = (char *)malloc(strlen(user_at_colon) + strlen(cwd) + strlen(prompt_suffix) + 1);
+	if (!shell->prompt)
+	{
+		perror("Error: Unable to allocate memory for the new prompt\n");
+		exit(EXIT_FAILURE);
+	}
+	strcpy(shell->prompt, user_at_colon);
+	strcat(shell->prompt, cwd);
+	strcat(shell->prompt, prompt_suffix);
+	printf("Updated prompt: %s\n", shell->prompt);
 }
 
-void update_prompt(t_shell *shell)
+void	update_prompt(t_shell *shell)
 {
-    char cwd[PATH_MAX];
-    get_current_directory(cwd);
+	char	cwd[PATH_MAX];
 
-    free(shell->prompt);
-
-    char *user = build_user_string();
-    char *user_at = build_user_at_string(user);
-    char *user_at_colon = build_user_at_colon_string(user_at);
-    char *prompt_suffix = build_prompt_suffix();
-
-    create_prompt(shell, user_at_colon, cwd, prompt_suffix);
-
-    free_memory(user, user_at, user_at_colon, prompt_suffix);
+	get_current_directory(cwd);
+	free(shell->prompt);
+	char	*user = build_user_string();
+	char	*user_at = build_user_at_string(user);
+	char	*user_at_colon = build_user_at_colon_string(user_at);
+	char	*prompt_suffix = build_prompt_suffix();
+	create_prompt(shell, user_at_colon, cwd, prompt_suffix);
+	free_memory(user, user_at, user_at_colon, prompt_suffix);
 }
