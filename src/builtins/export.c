2b680f5779df089b1601d0de982c04d8ee164a48
gbarone42 <badph3@gmail.com>
2024-01-26 15:48:39 +0100

    export.c norm OK

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -12,12 +12,12 @@
 
 #include "minishell.h"
 
-void process_env_variable(t_shell *shell, char *arg)
+void	process_env_variable(t_shell *shell, char *arg)
 {
-	char *name;
-	char *value;
+	char	*name;
+	char	*value;
 
-	name  = ft_strtok(arg, '=');
+	name = ft_strtok(arg, '=');
 	value = ft_strtok(NULL, '=');
 	if (name != NULL)
 	{
@@ -39,7 +39,7 @@ void process_env_variable(t_shell *shell, char *arg)
 		printf("Invalid export syntax: %s\n", arg);
 }
 
-void handle_export(t_shell *shell, char **args)
+void	handle_export(t_shell *shell, char **args)
 {
 	if (args[1] == NULL)
 	{
@@ -50,19 +50,3 @@ void handle_export(t_shell *shell, char **args)
 		process_env_variable(shell, args[1]);
 	}
 }
-
-
-// void handle_export(t_shell *shell, char **args)
-// void process_env_variable(t_shell *shell, char *arg)
-
-//v
-// void add_env_variable(t_shell *shell, const char *name, const char *value)
-// void add_new_variable(t_shell *shell, char *new_variable, int env_size)
-// int update_existing_variable(t_shell *shell, const char *name, char *new_variable)
-// char *create_new_env_variable(const char *name, const char *value)
-
-//vv
-// void add_envv_variable(t_shell *shell, const char *name, const char *value)
-// void add_new_envv_variable(t_shell *shell, const char *name, const char *value)
-// int find_envv_variable(t_shell *shell, const char *name, const char *value)
-// char *create_envv_variable(const char *name, const char *value)
